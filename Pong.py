ba07f524341592efba1f9dae574e9bcabe07d501
Scadrial <75407505+Scadrial@users.noreply.github.com>
2021-09-11 12:26:38 -0500

    Scoreboard
    Added a scoreboard so player A and player B can keep track of their scores.

--- Pong.py
+++ Pong.py
@@ -6,6 +6,10 @@ wn.bgcolor("black")
 wn.setup(width=800, height = 600)
 wn.tracer(0)
 
+#score
+score_a = 0
+score_b = 0
+
 #Paddle A
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
@@ -14,7 +18,6 @@ paddle_a.color("white")
 paddle_a.shapesize(stretch_wid=5,stretch_len=1)
 paddle_a.pu()
 paddle_a.goto(-350,0)
-
 
 #Paddle B
 paddle_b = turtle.Turtle()
@@ -34,6 +37,15 @@ ball.pu()
 ball.goto(0,0)
 ball.dx = 0.2
 ball.dy = 0.2
+
+#pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.pu()
+pen.hideturtle()
+pen.goto(0,260)
+pen.write("Player A: 0  Player B: 0", align = "center", font = ("Courier", 24, "normal"))
 
 #Functions
 def paddle_a_up():
@@ -55,6 +67,7 @@ def paddle_b_down():
     y = paddle_b.ycor()
     y = y - 20
     paddle_b.sety(y)
+
 #keyboard binding
 wn.listen()
 wn.onkeypress(paddle_a_up,'w')
@@ -81,14 +94,18 @@ while True:
     if ball.xcor() > 390:
         ball.goto(0,0)
         ball.dx *= -1
+        pen.write("Player A: {}  Player B: {}".format(score_a,score_b), align = "center", font = ("Courier", 24, "normal"))
     
     if ball.xcor() < -390:
         ball.goto(0,0)
         ball.dx *= -1
+        pen.write("Player A: {}  Player B: {}".format(score_a,score_b), align = "center", font = ("Courier", 24, "normal"))
+        
     #Paddle and ball collisions
     if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() - 40):
         ball.setx(340)
         ball.dx *= -1
+    
     if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() - 40):
         ball.setx(-340)
         ball.dx *= -1
